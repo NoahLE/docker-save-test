b4e9825f38514f870f3bc566d9ad4b6ab46dd127
NoahLE <noah@noahle.com>
2018-02-09 01:05:09 -0500

    added a sample app

---
+++ beepboop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BeepboopConfig(AppConfig):
+    name = 'beepboop'
--- composeexample/urls.py
+++ composeexample/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from beepboop.urls import index
 
 urlpatterns = [
+    path('', include('beepboop.urls')),
     path('admin/', admin.site.urls),
 ]
